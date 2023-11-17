2d057d8fb6a12b3af41aeb45f2645e6405d75239
Debo Dam <debojeetdam.business@gmail.com>
2023-11-16 19:06:27 -0700

    fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/homepage";
+// import { AuthProvider } from "./AuthContext";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <AuthProvider> */}
+      <Router>
+        <Routes>
+          <Route path="/homepage" element={<Homepage />} />
+        </Routes>
+      </Router>
+      {/* </AuthProvider> */}
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/homepage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const index = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default index;
