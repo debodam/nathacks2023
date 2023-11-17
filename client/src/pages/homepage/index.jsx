b26dc08e8d07e2220fae05476dc9eac7e541dbbe
Debo Dam <debojeetdam.business@gmail.com>
2023-11-16 22:16:04 -0700

    made front page

--- client/src/pages/homepage/index.jsx
+++ client/src/pages/homepage/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo_2.png";
 
 const index = () => {
   return (
@@ -9,14 +9,21 @@ const index = () => {
       <div className="hero">
         <div className="section__padding">
           <div className="hero-content">
-            <img src={logo} className="" alt="" />
-            {/* <button onClick={() => handleNavigate("/donate")}>
-              DONATE NOW
-            </button>
-            <button onClick={() => handleNavigate("/buynow")}>BUY NOW</button> */}
+            <img src={logo} className="hero-logo" alt="" />
           </div>
         </div>
+        <div className="hero-button">
+          <button className="button1">SIGN IN</button>
+          {/* You can add more buttons here */}
+        </div>
       </div>
+      {/* Connect Section */}
+      <div className="connect">
+        <div className="connect-button">
+          <button className="button1">CONNECT DEVICE</button>
+        </div>
+      </div>
+      <div className="scroll-down"></div>
     </div>
   );
 };
